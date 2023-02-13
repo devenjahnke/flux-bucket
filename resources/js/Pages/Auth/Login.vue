59ee9d56919430c26ce8a9c8a78755170258bb89
Deven Jahnke <10586846+devenjahnke@users.noreply.github.com>
2023-02-13 13:59:11 -0500

    chore: install prettier tailwind plugin
    Install the Prettier TailwindCSS plugin.
    Run prettier on existing application files.

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -31,7 +31,7 @@ const submit = () => {
 
         <div
             v-if="status"
-            class="mb-4 font-medium text-sm text-green-600"
+            class="mb-4 text-sm font-medium text-green-600"
         >
             {{ status }}
         </div>
@@ -80,7 +80,7 @@ const submit = () => {
                 />
             </div>
 
-            <div class="block mt-4">
+            <div class="mt-4 block">
                 <label class="flex items-center">
                     <Checkbox
                         name="remember"
@@ -90,11 +90,11 @@ const submit = () => {
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="mt-4 flex items-center justify-end">
                 <Link
                     v-if="canResetPassword"
                     :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    class="rounded-md text-sm text-gray-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                 >
                     Forgot your password?
                 </Link>
