2f28ad4d6eeb0ed9fee5fe0f92ce354dc446ef56
Deven Jahnke <10586846+devenjahnke@users.noreply.github.com>
2023-02-13 13:49:53 -0500

    chore: install typescript
    Install Typescript and the vue-tsc linter.
    Configure NPM to run the vue-tsc linter as part of the build process.
    Fix all issues identified by vue-tsc during build process.

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,18 +1,19 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
+import {createApp, DefineComponent, h} from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+import { Ziggy } from './ziggy'
 
-const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Flux Bucket';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    title: (title: string) => `${title} - ${appName}`,
+    resolve: (name: string) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')) as Promise<DefineComponent>,
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+        createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue, Ziggy)
             .mount(el);
